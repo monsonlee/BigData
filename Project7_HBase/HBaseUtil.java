501ded1752bd8eefc5ec0e4a0a3e962c8335bcd5
LiMengxian <imxlee@foxmail.com>
2016-12-22 22:38:30 +0800

    update

--- Project7_HBase/HBaseUtil.java
+++ Project7_HBase/HBaseUtil.java
@@ -4,9 +4,13 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+
+import static org.apache.hadoop.yarn.webapp.hamlet.HamletSpec.Scope.col;
 
 /**
  * HBase工具类
@@ -20,7 +24,7 @@ public class HBaseUtil {
      */
     public static Connection getConnection() throws IOException {
         Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "slave1,slave2,slave3");
+        conf.set("hbase.zookeeper.quorum", "crxy107");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
         return ConnectionFactory.createConnection(conf);
     }
@@ -61,5 +65,28 @@ public class HBaseUtil {
         Put put = new Put(rowKey);
         put.addColumn(familyName, columnName.getBytes(), columnValue.getBytes());
         return put;
+    }
+
+    /**
+     * 打印HBase查询结果
+     *
+     * @param result
+     */
+    public static void print(Result result) {
+        //result是个四元组<行键，列族，列(标记符)，值>
+        byte[] row = result.getRow(); //行键
+        NavigableMap<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> map = result.getMap();
+        for (Map.Entry<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> familyEntry : map.entrySet()) {
+            byte[] familyBytes = familyEntry.getKey(); //列族
+            for (Map.Entry<byte[], NavigableMap<Long, byte[]>> entry : familyEntry.getValue().entrySet()) {
+                byte[] column = entry.getKey(); //列
+                for (Map.Entry<Long, byte[]> longEntry : entry.getValue().entrySet()) {
+                    Long time = longEntry.getKey(); //时间戳
+                    byte[] value = longEntry.getValue(); //值
+                    System.out.println(String.format("行键rowKey=%s,列族columnFamily=%s,列column=%s,时间戳timestamp=%d,值value=%s", new String(row), new String(familyBytes), new String(column), time, new String(value)));
+                }
+            }
+        }
+
     }
 }
